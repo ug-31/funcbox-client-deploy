70225891c5100037579d889f534692abb67266d3
Ujjwal Gupta <ujjwalgupta3108@gmail.com>
2021-09-27 00:52:23 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Fragment, useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+
+import Navbar from "./components/Navigation.js";
+import Login from "./components/Login.js";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+import ListBucket from "./components/ListBucket";
+
+toast.configure();
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (Boolean) => {
+    setIsAuthenticated(Boolean);
+  };
+
+  const isAuth = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/auth/verify", {
+        method: "GET",
+        headers: { token: localStorage.token },
+      });
+
+      const parseRes = await response.json();
+
+      console.log(parseRes);
+
+      parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    isAuth();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar setAuth={setAuth} isAuthenticated={isAuthenticated} />
+      <Switch>
+        <Fragment>
+          <ToastContainer />
+
+          <Route
+            exact
+            path="/"
+            render={(props) =>
+              !isAuthenticated ? (
+                <Login {...props} setAuth={setAuth} />
+              ) : (
+                <Redirect to="/dashboard" />
+              )
+            }
+          />
+          <Route
+            exact
+            path="/login"
+            render={(props) =>
+              !isAuthenticated ? (
+                <Login {...props} setAuth={setAuth} />
+              ) : (
+                <Redirect to="/dashboard" />
+              )
+            }
+          />
+
+          <Route
+            exact
+            path="/dashboard"
+            render={(props) =>
+              isAuthenticated ? (
+                <Dashboard {...props} setAuth={setAuth} />
+              ) : (
+                <Redirect to="/login" />
+              )
+            }
+          />
+
+          <Route
+            exact
+            path="/register"
+            render={(props) =>
+              !isAuthenticated ? (
+                <Register {...props} setAuth={setAuth} />
+              ) : (
+                <Redirect to="/login" />
+              )
+            }
+          />
+
+          <Route
+            exact
+            path="/bucket"
+            render={(props) => (
+              <ListBucket
+                {...props}
+                setAuth={setAuth}
+                isAuthenticated={isAuthenticated}
+              />
+            )}
+          />
+        </Fragment>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,57 @@
+import React, { Fragment } from "react";
+import { toast } from "react-toastify";
+
+const Navigation = ({ setAuth, isAuthenticated }) => {
+  const onClickLogout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    setAuth(false);
+    toast.success("You Loggedout successfully");
+  };
+
+  return (
+    <Fragment>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="/dashboard">
+          Dashboard
+        </a>
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/bucket">
+              Bucket <span className="sr-only">(current)</span>
+            </a>
+          </li>
+        </ul>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              {!isAuthenticated === true && (
+                <a className="nav-link" href="/login">
+                  Login <span className="sr-only">(current)</span>
+                </a>
+              )}
+            </li>
+            <li className="nav-item active">
+              {!isAuthenticated === true && (
+                <a className="nav-link" href="/register">
+                  Register
+                </a>
+              )}
+            </li>
+
+            <li className="nav-item active">
+              {isAuthenticated === true && (
+                <a className="nav-link" href="/login" onClick={onClickLogout}>
+                  Logout
+                </a>
+              )}
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </Fragment>
+  );
+};
+
+export default Navigation;
